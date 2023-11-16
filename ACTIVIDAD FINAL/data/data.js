1463626c8c8bee59cf0797c02d970f680493ccba
IrisAviva <manucadavidcorrea@gmail.com>
2023-11-16 16:03:07 -0500

    actividad final manu

---
+++ ACTIVIDAD FINAL/data/data.js
@@ -0,0 +1,38 @@
+export const data=
+[
+    {
+        id: 1,
+        name: "Ficcion",
+       imagen: "./imagenes/1.jpeg"
+    },
+
+    {
+        id: 2,
+        name: "Poesía",
+        image: "./imagenes/2-Poesía.webp"
+    },
+
+    {
+        id: 3,
+        name: "Filosofía",
+        image: "/imagenes/3 - Filosofía.jpeg"
+    },
+
+    {
+        id: 4,
+        name: "Novela",
+        image: "/imagenes/4 - Novela.jpeg"
+    },
+
+    {
+        id: 5,
+        name: "autoayuda",
+        image: "/imagenes/5 - autoayuda.jpg"
+    },
+
+    {
+        "id": 6,
+        "name": "Thiller",
+        "image": "/imagenes/6 - thiller.jpeg"
+    },
+]
